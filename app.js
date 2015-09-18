beab7a53c2dd6e6da48917be7f773708f04c787d
Disco <jwo@umn.edu>
2015-09-18 13:28:10 -0500

    Minor script edit

--- app.js
+++ app.js
@@ -1,33 +1,24 @@
 $(document).ready(function() {
 
-	// Grab the template script
-  	var TemplateScript = $("#ColorTemplate").html();
-
-  	// Compile the template
-  	var Template = Handlebars.compile(TemplateScript);
-
 	$.ajax({
 		url: "data.json",
 		context: document.body
 	}).done(function(response) {
-		console.log(response);
 
-		// output using jQuery
-		// var i = 0;
-		// while (i < response.length) {
-		// 	$("#Main").append("<div class='ColorDiv' style='background-color:" + response[i].value + "'><p>" + response[i].color + "</p></div>");
-		// 	i++;
-		// }
+	    $.ajax({
+	        url: "Handlebars.hbs"
+	    }).done(function(HandlebarsData) {
 
-		// make object of returned data for Handlebars output
-		var wrapper  = {objects: response};	
+			var TemplateScript = HandlebarsData;
+			var Template = Handlebars.compile(TemplateScript);
 
-		// Pass our data to the template
- 		var CompiledHTML = Template(wrapper);
+	        var Wrapper = {objects: response};
+	        var CompiledHTML = Template(Wrapper);
 
-  		// Add the compiled html to the page
-  		$(".ContentPlaceholder").html(CompiledHTML);
+	        $('.ContentPlaceholder').html(CompiledHTML);
+
+		});
 
 	});
 
-})
+});
